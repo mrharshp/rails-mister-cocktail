07f9be8d8601f6381310864649906b6c17917386
Harsh Patel <harshp0301@gmail.com>
2019-05-15 11:43:28 +0100

    mister cocktail post heroku setup

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   # get 'cocktails/:id/doses/new'
   # post 'cocktails/:id/doses'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [:index, :show, :new, :create] do
+  root to 'cocktails#index'
+  resources :cocktails, only: [:show, :new, :create] do
     resources :doses, only: [:new, :create]
   end
   resources :doses, only: [:destroy]
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@
 require 'open-uri'
 require 'json'
 
+Ingredient.destroy_all
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 response = JSON.parse(open(url).read)
 
