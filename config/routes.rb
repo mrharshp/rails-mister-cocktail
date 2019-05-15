8fd35a81fad69d7d87d3ac1c8c78224d6672fa8b
Harsh Patel <harshp0301@gmail.com>
2019-05-15 10:54:18 +0100

    mister cocktail end of day 1 push

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  # delete 'doses/destroy'
+  # get 'cocktails/index'
+  # get 'cocktails/show'
+  # get 'cocktails/new'
+  # post 'cocktails/create'
+  # get 'cocktails/:id/doses/new'
+  # post 'cocktails/:id/doses'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: [:destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+response = JSON.parse(open(url).read)
+
+response['drinks'].each do |x|
+  Ingredient.create(name: x["strIngredient1"])
+end
